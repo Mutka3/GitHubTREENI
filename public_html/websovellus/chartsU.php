a7533718505be4ba2e8ff9e11e5d109e3e45d9bc
Maria Vanatoa <maria.vanatoa@metropolia.fi>
2019-04-25 16:00:56 +0300

    Update chartsU.php

--- public_html/websovellus/chartsU.php
+++ public_html/websovellus/chartsU.php
@@ -1,32 +1,47 @@
 <!doctype html public "-//w3c//dtd html 3.2//en">
 <html>
+
 <head>
-<title>User Pulse Data</title>
+    <title>User Pulse Data</title>
 
-<style>
+    <style>
+    @media only screen and (max-device-width: 1366px) {
+        .parallax {
+            background-attachment: scroll;
+        }
+    }
 
-body, html {
-  height: 100%;
-}
+    .chart {
+        width: 100%;
+        min-height: 450px;
+    }
 
-.parallax { 
-  /* The image used */
-  background-image: url("img/header-bg.jpg");
+    .row {
+        margin: 0 !important;
+    }
 
-  /* Full height */
-  height: 100%; 
+    .parallax {
+        /* The image used */
+        background-image: url("img/header-bg.jpg");
 
-  /* Create the parallax scrolling effect */
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
+        /* Full height */
+        height: 100%;
 
-</style>
+        /* Create the parallax scrolling effect */
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    h1 {
+        margin-left: 38%;
+    }
+    </style>
 </head>
-<body class = "parallax">
-<?Php
+
+<body class="parallax">
+    <?php
 require "config.php";// Database connection
 
 if($stmt = $connection->query("SELECT users.user_first AS usersFirst,
@@ -57,45 +72,71 @@ echo "<script>
 ?>
 
 
-<div id="chart_div"></div>
-<br><br>
-<a href=https://www.plus2net.com/php_tutorial/chart-column-database.php>Column Chart from MySQL database</a>
+    <div id="chart_div"></div>
+    <br><br>
+    <a href=https://www.plus2net.com/php_tutorial/chart-column-database.php>Column Chart from MySQL database</a> <script
+        type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {
+            packages: ['corechart', 'bar']
+        });
+        google.charts.setOnLoadCallback(drawChart1);
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
+        function drawChart1() {
 
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {packages: ['corechart', 'bar']});
-      google.charts.setOnLoadCallback(drawChart);
-	  
-      function drawChart() {
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'User');
+            data.addColumn('number', 'Pulse');
+            data.addColumn('number', 'Date');
+            for (i = 0; i < my_2d.length; i++)
+                data.addRow([my_2d[i][0], parseInt(my_2d[i][1]), parseInt(my_2d[i][2])]);
+            var options = {
+                title: 'Pulse Data',
+                hAxis: {
+                    title: 'Pulse Data',
+                    titleTextStyle: {
+                    color: '#333'
+                    }
+                },
+                vAxis: {
+                    minValue: 0
+                }
+            };
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'User');
-        data.addColumn('number', 'Pulse');
-		data.addColumn('number', 'Date');
-        for(i = 0; i < my_2d.length; i++)
-    data.addRow([my_2d[i][0], parseInt(my_2d[i][1]),parseInt(my_2d[i][2])]);
-       var options = {
-          title: 'Pulse Data',
-          hAxis: {title: 'Pulse Data',  titleTextStyle: {color: '#333'}},
-          vAxis: {minValue: 0}
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('chart_div'));
-        chart.draw(data, options);
-       }
-
-       
-	///////////////////////////////
-////////////////////////////////////	
-</script>
-</body></html>
-
-
-
+            var chart = new google.charts.Bar(document.getElementById('chart_div1'));
+            chart.draw(data, options);
+        }
 
 
+        $(window).resize(function() {
+            drawChart1();
+        });
+        ///////////////////////////////
+        ////////////////////////////////////	
+        </script>
+
+        </head>
+
+        <body>
+            <!--Div that will hold the pie chart-->
 
 
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1>User heart rate data</h1>
+
+                </div>
+                <div class="col-md-4 col-md-offset-4">
+                    <hr />
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-md-6">
+                    <div id="chart_div1" class="chart"></div><hr />
+                </div>
+            </div>
+            </div>
+        </body>
+
+</html>
